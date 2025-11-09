f5a52167265ecbcc5879df2cac3e74fb4c7533b9
siam4201 <siamsarker36@gmail.com>
2025-11-09 10:49:21 +0600

    Upload

---
+++ Mid/Lab Task 2/Clinic Patient Registration.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html>
+
+<style>
+.Div {
+  
+  background-color: lightgray;
+
+  width: 600px;
+  margin: 0 auto;
+  padding: 20px 10px 10px
+  
+  
+}
+</style>
+    <head>
+        <title>Clinic Patient Registration</title>
+    </head>
+    <body>
+        <center>
+        <h1>Clinic Patient Registration</h1>
+</center>
+<div class="Div">
+Full Name:<br>
+    <input type = "text"><br><br>
+
+    Age:<br>
+    <input type = "text"><br><br>
+
+    Phone Number:<br>
+    <input type = "text"><br><br>
+
+    Email Address:<br>
+    <input type = "text"><br><br>
+
+    Insurance Provider:<br>
+    <input type = "text"><br><br>
+
+      <label for="Insurance">Insurance Provider:</label><br>
+
+<select name="Insurance" id="Insurance">
+  <option value="Select Provider">Select Provider</option>
+  <option value="2">1</option>
+  <option value="3">2</option>
+  <option value="4">3</option>
+</select><br><br>
+
+ Insurance Policy Number:<br>
+    <input type = "text"><br><br>
+
+    <center>
+    <h2>Additional Information<h2>
+</center>
+    USername:<br>
+    <input type = "text"><br><br>
+
+    Password:<br>
+    <input type = "text"><br><br>
+
+    Confirm Password:<br>
+    <input type = "text"><br><br>
+
+    <center>
+    <button>Register</button>
+    <center>
+
+</div> 
+    
+
+    </body>
+</html>
